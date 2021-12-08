3d208931ff69ba1824be4bee3ef5180b53585945
azhar.yusof <azhar.yusof@edgenta.com>
2021-12-08 12:43:41 +0800

    new commit

---
+++ Controllers/StaffController.cs
@@ -0,0 +1,22 @@
+﻿using DotNetCoreSQL.Context;
+using DotNetCoreSQL.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCoreSQL.Controllers
+{
+    public class StaffController : Controller
+    {
+        readonly StaffDBContext staffDBContext = new StaffDBContext();
+
+        public IActionResult Index()
+        {
+            List<StaffInfo> StaffInfoList = staffDBContext.GetStaffInfo().ToList();
+
+            return View(StaffInfoList);
+        }
+    }
+}
